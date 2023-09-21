d13777b8d28d5594e068a6ac624eef298a9e4c49
kidattche <kidattche200577@gmail.com>
2023-09-22 08:44:38 +0900

    コンポーネントの作成完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if($(".js-hamburger").hasClass('is-active')){
+    $('.js-hamburger').removeClass("is-active");
+    // $("html").toggleClass("is-fixed");
+    $(".js-sp-nav").fadeOut();
+   } else {
+    $(".js-hamburger").addClass("is-active");
+    $(".js-sp-nav").fadeIn();
+
+   }
+  });
 
 
 });
