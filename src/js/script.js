965377886523d1d1d230d604a9b831c92b48b797
kidattche <kidattche200577@gmail.com>
2023-09-27 13:33:33 +0900

    スワイパー途中

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,36 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
 });
+//ローディング
+$(function () {
+  function end_loader() {
+    $('.loader').fadeOut(800);
+  }
+  $(window).on('load', function () {
+    setTimeout(function () {
+      end_loader();
+    }, 3000)
+  })
+})
+
+// メインビューのスワイパー
+window.addEventListener('load', () => {
+  const elem = document.querySelector('.p-swiper-container');
+  if (elem === null) return;
+  const swiperParams = {
+    loop: true, // ループさせる
+    effect: 'fade', // フェードのエフェクト
+    autoplay: {
+      delay: 4000, // ４秒後に次の画像へ
+      disableOnInteraction: false, // ユーザー操作後に自動再生を再開する
+    },
+    speed: 2000, // ２秒かけながら次の画像へ移動
+    allowTouchMove: false, // マウスでのスワイプを禁止
+    navigation: {
+      // 左右のページ送りを有効にする
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  };
+  const swiper = new Swiper('.p-swiper-container', swiperParams);
+});
