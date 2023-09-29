be9e505979c0089e4e603f96f16011c6a6354ba3
kidattche <kidattche200577@gmail.com>
2023-09-30 05:40:54 +0900

    ボタンのアニメーションまで

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
 });
+
 //ローディング
 $(function () {
   function end_loader() {
@@ -27,7 +28,7 @@ $(function () {
   })
 })
 
-// メインビューのスワイパー
+//swiper
 window.addEventListener('load', () => {
   const elem = document.querySelector('.p-swiper-container');
   if (elem === null) return;
@@ -47,4 +48,50 @@ window.addEventListener('load', () => {
     },
   };
   const swiper = new Swiper('.p-swiper-container', swiperParams);
+});
+
+// Campaignスライド
+
+const swiper = new Swiper('.js-campaign-swiper', {
+  loop: true,
+  autoplay: {
+      delay: 0,
+  },
+  autoWidth: true,
+  autoHeight: true,
+  speed: 3000,          //追記
+  slidesPerView: 4,     //追記
+  navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	}
+})
+
+
+
+//アニメーション
+//要素の取得とスピードの設定
+var box = $('.informationーimg'),
+    speed = 700;  
+ 
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+ 
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+    $(this).delay(200).animate({'width':'100%'},speed,function(){
+                   image.css('opacity','1');
+                   $(this).css({'left':'0' , 'right':'auto'});
+                   $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+          }
+     });
 });
